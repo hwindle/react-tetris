fe6063c101a715bf437881b80bc4de70823afa1d
Hazel Windle <hwindle83@googlemail.com>
2023-03-08 18:37:19 +0000

    Completed styled cells/pieces

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { StyledCell } from './styles/StyledCell';
+import { PIECES } from '../tetrisPieces';
 
 const Cell = ({ type }) => (
-  <div>cell</div>
+  <StyledCell type={type} colour={PIECES[type].colour}>cell</StyledCell>
 );
 
 export default Cell;
