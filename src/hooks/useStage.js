0934743051a77d50864b46b97b6e4e8abab2c181
Hazel Windle <hwindle83@googlemail.com>
2023-03-16 13:37:43 +0000

    Finished the row removing feature

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -3,8 +3,23 @@ import { createStage } from '../gameHelpers';
 
 export const useStage = (player, resetPlayer) => {
   const [stage, setStage] = useState(createStage());
+  const [rowsCleared, setRowsCleared] = useState(0);
 
   useEffect(() => {
+    setRowsCleared(0);
+    const sweepRows = newStage => newStage.reduce((acc, row) => {
+      // looking for full rows without 0
+      if (row.findIndex(cell => cell[0] === 0)  === -1) {
+        setRowsCleared(prev => prev + 1);
+        // insert value at start of array
+        acc.unshift(new Array(newStage[0].length).fill([0, 'clear']));
+        return acc;
+      }
+      // empty spaces remaining in row
+      acc.push(row);
+      return acc;
+    }, []);
+
     const updateStage = (prevStage) => {
       // create a fresh, new stage
       const newStage = prevStage.map((row) =>
@@ -26,6 +41,7 @@ export const useStage = (player, resetPlayer) => {
       // check player collision with walls, other tetris pieces
       if (player.collided) {
         resetPlayer();
+        return sweepRows(newStage);
       }
       // return newStage render
       return newStage;
